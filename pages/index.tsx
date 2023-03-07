8b3fb494e546c15b85899327108da032e13df15d
noahsb96 <noahsb96@yahoo.com>
2023-03-07 15:42:36 -0700

    Installed simple typewriter and Added text

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,35 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import Header from '@/components/Header'
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import styles from '@/styles/Home.module.css';
+import Header from '@/components/Header';
+import Hero from '@/components/Hero';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Noah Bruce</title>
-      </Head>
-      
-      {/* Header */}
-      <Header />
+	return (
+		<>
+			<Head>
+				<title>Noah Bruce</title>
+			</Head>
 
-      {/* Hero */}
+			<Header />
 
-      {/* About */}
+			{/* Hero */}
+      <section id='hero'>
+        <Hero />
+      </section>
 
-      {/* Experience */}
+			{/* About */}
 
-      {/* Skills */}
+			{/* Experience */}
 
-      {/* Projects */}
+			{/* Skills */}
 
-      {/* Contact Me */}
-    </>
-  )
+			{/* Projects */}
+
+			{/* Contact Me */}
+		</>
+	);
 }
