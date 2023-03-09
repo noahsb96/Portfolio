d3e3b4c06353eb185c7838c27b8fc7235d332ace
noahsb96 <noahsb96@yahoo.com>
2023-03-08 21:16:29 -0700

    Added picture and responsive design to picture to about page

--- components/About.tsx
+++ components/About.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+import Headshot from '../images/Headshot.jpg';
+import Head from 'next/head';
 
 type Props = {};
 
@@ -8,6 +11,20 @@ function About({}: Props) {
 			<h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
 				About
 			</h3>
+
+			<motion.img
+				src={Headshot.src}
+				initial={{
+					x: -200,
+					opacity: 0,
+				}}
+				transition={{
+					duration: 1.2,
+				}}
+				whileInView={{ opacity: 1, x: 0 }}
+				viewport={{ once: true }}
+				className='-mb-20 md:mb-0 flex-shrink-0 h-80 w-80 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]'
+			/>
 		</div>
 	);
 }
