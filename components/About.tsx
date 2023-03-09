777a74a05a8e87a1c0546508cb2c77f5fff2e0d4
noahsb96 <noahsb96@yahoo.com>
2023-03-08 21:44:45 -0700

    going to add text to bottom of picture

--- components/About.tsx
+++ components/About.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import Headshot from '../images/Headshot.jpg';
-import Head from 'next/head';
 
 type Props = {};
 
 function About({}: Props) {
 	return (
-		<div className='flex flex-xol relative h-screen text-xenter md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
+		<div className='flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
 			<h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
 				About
 			</h3>
@@ -25,6 +24,10 @@ function About({}: Props) {
 				viewport={{ once: true }}
 				className='-mb-20 md:mb-0 flex-shrink-0 h-80 w-80 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]'
 			/>
+
+			<div>
+				<h4></h4>
+			</div>
 		</div>
 	);
 }
