dd45da6b09060b8c67cf8e44762fe9b6f5f44d3e
noahsb96 <noahsb96@yahoo.com>
2023-03-09 19:48:19 -0700

    added animation to picture

--- components/About.tsx
+++ components/About.tsx
@@ -6,8 +6,12 @@ type Props = {};
 
 function About({}: Props) {
 	return (
-		<div className='flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
-			<h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
+		<motion.div
+			initial={{ opacity: 0 }}
+			whileInView={{ opacity: 1 }}
+			transition={{ duration: 1.5 }}
+			className='flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
+			<h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl :'>
 				About
 			</h3>
 
@@ -22,7 +26,7 @@ function About({}: Props) {
 				whileInView={{ opacity: 1, x: 0 }}
 				viewport={{ once: true }}
 				src={Headshot.src}
-				className='-mb-20 md:mb-0 flex-shrink-0 w-80 h-80 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]'
+				className='-mb-20 md:mb-0 flex-shrink-0 w-60 h-60 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]'
 			/>
 
 			<div className='space-y-10 px-0 md:px-10'>
@@ -31,7 +35,7 @@ function About({}: Props) {
 					<span className='underline decoration-[#F7AB0A]/50'>little</span>{' '}
 					background
 				</h4>
-				<p className='text-sm'>
+				<p className='text-base'>
 					I am a software engineer and a musician. I've put my heart and soul
 					into my two passions. Music and technology. Whether it be playing the
 					drums across the US or learning HTML, CSS and JavaScript, I've taken
@@ -42,7 +46,7 @@ function About({}: Props) {
 					deliver the best product possible.
 				</p>
 			</div>
-		</div>
+		</motion.div>
 	);
 }
 
