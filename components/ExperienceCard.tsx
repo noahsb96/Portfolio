ee2f988cd3321aa083850ec8e36fdf4cfd728692
noahsb96 <noahsb96@yahoo.com>
2023-03-10 23:08:27 -0700

    Adding logos to experience card

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from 'react';
+import Petes from '../images/Petes.jpg';
+import { motion } from 'framer-motion';
+import Image from 'next/image';
 
-type Props = {}
+type Props = {};
 
 function ExperienceCard({}: Props) {
-  return (
-    <div>ExperienceCard</div>
-  )
+	return (
+		<article>
+			<motion.img
+				initial={{
+					y: -100,
+					opacity: 0,
+				}}
+				transition={{ duration: 1.2 }}
+				whileInView={{ opacity: 1, y: 0 }}
+				viewport={{ once: true }}
+				className='w-32 h-32 rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center'
+				src={Petes.src}
+				alt=''
+			/>
+		</article>
+	);
 }
 
-export default ExperienceCard
+export default ExperienceCard;
