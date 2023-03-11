a968dad202d2e9f46c1c2ea7452d0b4c85405200
noahsb96 <noahsb96@yahoo.com>
2023-03-10 23:20:12 -0700

    finishing up work experience cards

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -7,7 +7,7 @@ type Props = {};
 
 function ExperienceCard({}: Props) {
 	return (
-		<article>
+		<article className='flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px]'>
 			<motion.img
 				initial={{
 					y: -100,
@@ -20,6 +20,22 @@ function ExperienceCard({}: Props) {
 				src={Petes.src}
 				alt=''
 			/>
+
+			<div className='px-0 md:px-10'>
+				<h4 className='text-4xl font-light'>
+					Front Line / Back of House / Shift Supervisor / Bartender
+				</h4>
+				<p className='font-bold text-2xl mt-1'>Illegal Pete's</p>
+				<p className='uppercase py-5 text-gray-300'>
+					Started work... - Ended...
+				</p>
+				<ul className='list-disc space-y-4 ml-5 text-lg'>
+					<li>Summary Points</li>
+					<li>Summary Points</li>
+					<li>Summary Points</li>
+					<li>Summary Points</li>
+				</ul>
+			</div>
 		</article>
 	);
 }
