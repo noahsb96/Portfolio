3d60b0e8acc5a0438c1ec9f6b8548348aab5c9b4
noahsb96 <noahsb96@yahoo.com>
2023-03-14 23:07:14 -0600

    added styling to projects scroll

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -12,13 +12,15 @@ function Projects({}: Props) {
 
 			<div className='relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20'>
 				{projects.map((project) => (
-					<div>
-						<img 
-						src='http://cdn.sanity.io/images/ltuexkre/production/af7ca99b5a796d0698cf9121a4a0795b5022b6be-666x375.png'
-						alt=''
+					<div className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen'>
+						<img
+							src='http://cdn.sanity.io/images/ltuexkre/production/af7ca99b5a796d0698cf9121a4a0795b5022b6be-666x375.png'
+							alt=''
 						/>
 
-						<div></div>
+						<div>
+							<h4>Case Study 1 of 3: UPS clone</h4>
+						</div>
 					</div>
 				))}
 			</div>
