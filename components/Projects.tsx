edc1d980a9ba96c82ac4ff0a10a143b2cbcb2a95
noahsb96 <noahsb96@yahoo.com>
2023-03-22 20:42:42 -0600

    Forgot to change scrollbar on projects

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -11,7 +11,7 @@ function Projects({}: Props) {
 				Projects
 			</h3>
 
-			<div className='relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20'>
+			<div className='relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80'>
 				{projects.map((project, i) => (
 					<motion.div
 						initial={{ opacity: 0 }}
