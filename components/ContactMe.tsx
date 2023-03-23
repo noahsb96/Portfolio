8cfd3be1a1418a868894572285782b0c33f4beb1
noahsb96 <noahsb96@yahoo.com>
2023-03-22 20:07:33 -0600

    Added email client functionality to contact form to open personal email client on submit and have email outline pop up

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -14,7 +14,9 @@ type Props = {};
 function ContactMe({}: Props) {
 	const { register, handleSubmit } = useForm<Inputs>();
 
-	const onSubmit: SubmitHandler<Inputs> = (formData) => console.log(formData);
+	const onSubmit: SubmitHandler<Inputs> = (formData) => {
+		window.location.href = `mailto:noahsb96@yahoo.com?subject=${formData.subject}&body=Hi, my name is ${formData.name}. ${formData.message} (${formData.email})`;
+	};
 	return (
 		<div className='h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
 			<h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
