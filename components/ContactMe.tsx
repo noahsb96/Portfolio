6909672d4f1eec5d195c02fe4dfebefc5aad5a14
noahsb96 <noahsb96@yahoo.com>
2023-03-22 19:54:26 -0600

    Added onSubmit to form in contact page

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -12,13 +12,9 @@ type Inputs = {
 type Props = {};
 
 function ContactMe({}: Props) {
-	const {
-		register,
-		handleSubmit,
-		watch,
-		formState: { errors },
-	} = useForm<Inputs>();
-	const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+	const { register, handleSubmit } = useForm<Inputs>();
+
+	const onSubmit: SubmitHandler<Inputs> = (formData) => console.log(formData);
 	return (
 		<div className='h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
 			<h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
@@ -48,7 +44,9 @@ function ContactMe({}: Props) {
 					</div>
 				</div>
 
-				<form className='flex flex-col space-y-2 w-fit mx-auto'>
+				<form
+					onSubmit={handleSubmit(onSubmit)}
+					className='flex flex-col space-y-2 w-fit mx-auto'>
 					<div className='flex space-x-2'>
 						<input
 							placeholder='Name'
