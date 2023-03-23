b929c8515fa1486f0c3d7d5ee0503b4b80143aba
noahsb96 <noahsb96@yahoo.com>
2023-03-22 19:50:37 -0600

    adding react hooks form import and declaring variables

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import { PhoneIcon, MapPinIcon, EnvelopeIcon } from '@heroicons/react/24/solid';
+import { useForm, SubmitHandler } from 'react-hook-form';
+
+type Inputs = {
+	example: string;
+	exampleRequired: string;
+};
 
 type Props = {};
 
 function ContactMe({}: Props) {
+	const {
+		register,
+		handleSubmit,
+		watch,
+		formState: { errors },
+	} = useForm<Inputs>();
+	const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
 	return (
 		<div className='h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
 			<h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
