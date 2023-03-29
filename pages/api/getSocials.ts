04b7643d757581e4584fc1e3034b54acfa15fc9e
noahsb96 <noahsb96@yahoo.com>
2023-03-28 20:36:07 -0600

    put in api endpoints to get socials. Have to create typings to get rid of type errors

--- pages/api/getSocials.ts
+++ pages/api/getSocials.ts
@@ -1,3 +1,19 @@
 import type { NextApiRequest } from "next";
 import { groq } from "next-sanity";
 import { sanityClient } from "@/sanity";
+
+const query = groq`
+    *[_type == 'social']
+`
+
+type Data = {
+    socials: Social[]
+}
+
+export default function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
+  ) {
+    const socials: Social[] = await sanityClient.fetch(query);
+    res.status(200).json({ socials })
+  }
