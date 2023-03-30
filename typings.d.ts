0d77e86ee2ef20d1e91af4ef0f59f18aaae35103
noahsb96 <noahsb96@yahoo.com>
2023-03-29 22:10:51 -0600

    Added Image interface in declaration file

--- typings.d.ts
+++ typings.d.ts
@@ -1,12 +1,20 @@
 interface SanityBody {
-    _createdAt: string;
-    _id: string;
-    _rev: string;
-    _updatedAt: string;
+	_createdAt: string;
+	_id: string;
+	_rev: string;
+	_updatedAt: string;
+}
+
+interface Image {
+	_type: 'image';
+	asset: {
+		_ref: string;
+		_type: 'reference';
+	};
 }
 
 export interface Social extends SanityBody {
-    _type: 'social';
-    title: string;
-    url: string;
+	_type: 'social';
+	title: string;
+	url: string;
 }
