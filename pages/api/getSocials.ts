972190f2a30c1173a611d6583e52284f1110c8b9
noahsb96 <noahsb96@yahoo.com>
2023-03-29 22:09:47 -0600

    Added default interface and social interface in declaration file

--- pages/api/getSocials.ts
+++ pages/api/getSocials.ts
@@ -1,4 +1,4 @@
-import type { NextApiRequest } from "next";
+import type { NextApiRequest, NextApiResponse } from "next";
 import { groq } from "next-sanity";
 import { sanityClient } from "@/sanity";
 
