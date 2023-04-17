8a2f7213445f28fb5bbbe5ff590a225015209951
noahsb96 <noahsb96@yahoo.com>
2023-04-16 22:12:03 -0600

    Successfully added heroImage dynamically

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Cursor, useTypewriter } from 'react-simple-typewriter';
 import BackgroundCircles from './BackgroundCircles';
-import Headshot from '../images/Headshot.jpg';
 import Image from 'next/image';
 import Link from 'next/link';
 import { PageInfo } from '@/typings';
+import { urlFor } from '@/sanity';
 
 type Props = {
 	pageInfo: PageInfo;
@@ -12,17 +12,27 @@ type Props = {
 
 export default function Hero({ pageInfo }: Props) {
 	const [text, count] = useTypewriter({
-		words: [`Hi, My Name's ${pageInfo?.name}`, 'Developer', 'Designer', 'Creator'],
+		words: [
+			`Hi, My Name's ${pageInfo?.name}`,
+			'Developer',
+			'Designer',
+			'Creator',
+		],
 		loop: true,
 		delaySpeed: 2000,
 	});
+
+	const src = urlFor(pageInfo?.heroImage).url();
 
 	return (
 		<div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
 			<BackgroundCircles />
 			<Image
+				loader={() => src}
 				className='relative rounded-full h-32 w-32 mx-auto object-cover'
-				src={Headshot}
+				src={src}
+				width={32}
+				height={32}
 				alt=''
 			/>
 			<div className='z-20'>
