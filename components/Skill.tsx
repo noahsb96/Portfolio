9dd68afdadd3939427d0fad07b56ef3182e95c15
noahsb96 <noahsb96@yahoo.com>
2023-04-24 14:50:38 -0600

    still running into errors for pageinfo. Trying to solve

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -24,7 +24,7 @@ function Skill({ skill, directionLeft }: Props) {
 			<div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white w-15 h-15 md:w-24 md:h-24 xl:w-26 xl:h-26 rounded-full z-0'>
 				<div className='flex items-center justify-center h-full'>
 					<p className='text-3xl font-bold text-black opacity-100'>
-						{skill.progress}
+						{skill.progress}%
 					</p>
 				</div>
 			</div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ import { fetchProjects } from '@/utils/fetchProjects';
 import { fetchSocials } from '@/utils/fetchSocials';
 
 type Props = {
-	pageInfo: PageInfo[];
+	pageInfo: PageInfo;
 	experiences: Experience[];
 	skills: Skill[];
 	projects: Project[];
@@ -86,11 +86,11 @@ const Home = ({ pageInfo, experiences, skills, projects, socials }: Props) => {
 export default Home;
 
 export const getStaticProps: GetStaticProps<Props> = async () => {
-	const pageInfo: PageInfo[] = await fetchPageInfo();
-	const experiences: Experience[] = await fetchExperiences();
-	const skills: Skill[] = await fetchSkills();
-	const projects: Project[] = await fetchProjects();
-	const socials: Social[] = await fetchSocials();
+	const pageInfo = await fetchPageInfo();
+	const experiences = await fetchExperiences();
+	const skills = await fetchSkills();
+	const projects = await fetchProjects();
+	const socials = await fetchSocials();
 
 	return {
 		props: {
@@ -100,6 +100,6 @@ export const getStaticProps: GetStaticProps<Props> = async () => {
 			projects,
 			socials,
 		},
-		revalidate: 3600,
+		revalidate: 10,
 	};
 };
