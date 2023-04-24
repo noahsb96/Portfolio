a2e7e6eb4980216ae6697a4bb901cfce6e64032d
noahsb96 <noahsb96@yahoo.com>
2023-04-23 21:48:51 -0600

    fixed responsive design on skills on medium screens

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -19,7 +19,7 @@ function Skill({ skill, directionLeft }: Props) {
 				transition={{ duration: 1 }}
 				whileInView={{ opacity: 1, x: 0 }}
 				src={urlFor(skill?.image).url()}
-				className='rounded-full border border-gray-500 object-cover w-15 h-15 md:w-28 md:h-28 xl:w-26 xl:h-26 filter group-hover:grayscale transition duration-300 ease-in-out'
+				className='rounded-full border border-gray-500 object-cover w-15 h-15 md:w-24 md:h-24 xl:w-26 xl:h-26 filter group-hover:grayscale transition duration-300 ease-in-out'
 			/>
 			<div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-15 w-15 md:w-28 md:h-28 xl:w-26 xl:h-26 rounded-full z-0'>
 				<div className='flex items-center justify-center h-full'>
