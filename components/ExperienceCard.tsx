afd4e67142dfd9987a4c6cc8c26aa5b17adc667d
noahsb96 <noahsb96@yahoo.com>
2023-04-23 21:58:49 -0600

    fixed responsive design for medium screens in projects

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -10,7 +10,7 @@ type Props = {
 
 function ExperienceCard({ experience }: Props) {
 	return (
-		<article className='flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-{#292929] p-10 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden'>
+		<article className='flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[800px] xl:w-[900px] snap-center bg-{#292929] p-10 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden'>
 			<motion.img
 				initial={{
 					y: -100,
